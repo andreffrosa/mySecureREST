6775802b2b55c682141faedb78e2b8985e30508a
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2019-05-19 21:45:36 +0100

    State saving

--- src/http/Entity.java
+++ src/http/Entity.java
@@ -2,4 +2,27 @@ package http;
 
 public class Entity {
 
+	
+	
+	private byte[] rawData;
+	private String contentType;
+	
+	
+	private Entity(byte[] rawData, String contentType ) {
+		this.rawData = rawData;
+		this.contentType = contentType;
+	}
+	
+	public static Entity entity(Object object) {
+		
+		if (object instanceof String) {
+			return new Entity( ((String) object).getBytes(), MediaType.TEXT_PLAIN); 		
+		
+		}else if(object instanceof byte[])
+			return new Entity ( (byte[]) object, MediaType.APPLICATION_OCTET_STREAM);
+		
+		}else
+			return new Entity( );
+}
+	
 }
