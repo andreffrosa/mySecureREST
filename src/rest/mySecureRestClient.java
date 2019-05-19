12c317630732b13ffcb3724de55c0fe7d0714b82
André Rosa <af.rosa@campus.fct.unl.pt>
2019-05-19 20:23:52 +0100

    cleaned code

--- src/rest/mySecureRestClient.java
+++ src/rest/mySecureRestClient.java
@@ -1,31 +1,19 @@
 package rest;
 
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
+import java.net.Socket;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.security.KeyManagementException;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.KeyManagerFactory;
+import javax.net.SocketFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
 
 import http.HTTPReply;
 import http.HTTPRequest;
@@ -33,21 +21,29 @@ import http.Parser;
 
 public class mySecureRestClient {
 
-	SSLSocketFactory socket_factory;
+	SocketFactory socket_factory;
 	URL endpoint;
 
-	public mySecureRestClient() {
+	public mySecureRestClient() throws NoSuchAlgorithmException {
+		this.socket_factory = SSLContext.getDefault().getSocketFactory();
 	}
 	
 	public mySecureRestClient(String location) throws UnknownHostException, IOException, UnrecoverableKeyException, KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException {
+		this(SSLContext.getDefault().getSocketFactory(), location);
+	}
+	
+	public mySecureRestClient(SocketFactory socket_factory, String location) throws UnknownHostException, IOException, UnrecoverableKeyException, KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException {
+		this.socket_factory = socket_factory;
 		setLocation(location);
 	}
 	
-	private SSLSocket newSocket(String endpoint, int port) throws UnknownHostException, IOException {
+	private Socket newSocket(String endpoint, int port) throws UnknownHostException, IOException {
 				
-		SSLSocket socket = (SSLSocket) this.socket_factory.createSocket(endpoint, port);
+		Socket socket = this.socket_factory.createSocket(endpoint, port);
 		
-		socket.startHandshake();
+		if( socket instanceof SSLSocket )
+			((SSLSocket) socket).startHandshake();
+		
 		return socket;
 	}
 	
@@ -64,7 +60,7 @@ public class mySecureRestClient {
 		
 		System.out.println(new String(request.serialize()));
 	    
-		SSLSocket socket = newSocket(endpoint.getHost(), endpoint.getPort());
+		Socket socket = newSocket(endpoint.getHost(), endpoint.getPort());
 		
 	    socket.getOutputStream().write(request.serialize());
 	    
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,16 @@
 package test;
 
+import java.io.FileInputStream;
 import java.net.InetAddress;
 import java.net.URL;
+import java.security.KeyStore;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 import http.HTTPReply;
 import rest.mySecureRestClient;
+import ssl.CustomSSLSocketFactory;
 
 public class Test {
 
@@ -13,7 +19,17 @@ public class Test {
 		
 		//System.setProperty("javax.net.ssl.trustStrore", "../SRSC-Proj2/configs/client/client-truststore.pkcs12");
 		
-		mySecureRestClient client = new mySecureRestClient(location);
+		String ks_password = "SRSC1819";
+		
+		KeyStore ks = KeyStore.getInstance("PKCS12");
+		ks.load(new FileInputStream("../SRSC-Proj2/configs/fServer/mainDispatcher/mainDispatcher-keystore.pkcs12"), ks_password.toCharArray());
+		
+		KeyStore ts = KeyStore.getInstance("PKCS12");
+		ts.load(new FileInputStream("../SRSC-Proj2/configs/fServer/mainDispatcher/mainDispatcher-truststore.pkcs12"), ks_password.toCharArray());
+		
+		SSLSocketFactory factory = new CustomSSLSocketFactory(ks, ks_password, ts);
+		
+		mySecureRestClient client = new mySecureRestClient(factory, location);
 		
 		HTTPReply reply = client.post("/", "text/html; charset=utf-8", "ola".getBytes());
 		
