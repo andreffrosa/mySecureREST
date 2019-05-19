af9e2fca4330d499c790df9f839aa4a306e5fc0c
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2019-05-19 16:56:26 +0100

    Beauty pt 2

--- src/http/Parser.java
+++ src/http/Parser.java
@@ -1,29 +1,86 @@
-/**
- * 
- */
 package http;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * @author ruben
- *
- */
-public interface Parser {
+public class Parser {
+
+	private static final int METHOD = 0;
+	private static final int PATH = 1;
+	private static final int REQUEST_HTTP_VERSION = 2;
 	
-	/**
-	 * Serialize an HttpMessage to byte array to send data through a socket. 
-	 * @param httpMessage Message to send.
-	 * @return The message in a byte array form factor.
-	 */
-	public byte[] serialize( HTTPMessage httpMessage )  throws IOException;
 	
-	/**
-	 * Returns a HttpMessage from raw data that arrived on the socket.
-	 * @param in Inputstream of the socket
-	 * @return The message.
-	 */
-	public HTTPMessage desseralizeReply(InputStream in )  throws IOException;
+	private static final int RESPONSE_HTTP_VERSION = 0;
+	private static final int CODE = 1;
+	private static final int MESSAGE = 2;
+
+	public Parser() {	}
+
+
+	public static HTTPReply desseralizeReply(InputStream in) throws IOException {
+
+		Map<String, String> headers = new HashMap<>();
+
+		String[] responseStatus = readLine(in).split(" ", 2); // [HttpVersion, Code, Message]	
+		headers = readHeaders(in);
+		byte[] buffer = extractBody(in, headers);
+		
+		return new HTTPReply(responseStatus[RESPONSE_HTTP_VERSION], Integer.parseInt(responseStatus[CODE]), responseStatus[MESSAGE], headers, buffer);
+	}
+	
+	public static HTTPRequest desserializeRequest(InputStream in) throws IOException {
+		
+		Map<String, String> headers ;
+
+		String[] responseStatus = readLine(in).split(" "); // [Method, Path, HttpVersion]	
+		
+		//Process headers
+		headers = readHeaders(in);
+		byte[] buffer = extractBody(in, headers);
+		
+		return new HTTPRequest( responseStatus[METHOD], responseStatus[PATH], responseStatus[REQUEST_HTTP_VERSION], headers, buffer);
+		
+	}
+
+	private static byte[] extractBody(InputStream in, Map<String, String> headers) throws IOException {
+		int size = Integer.parseInt( headers.get("Content-Length") );
+		byte[] buffer = new byte[size];
+
+		int n = 0;
+		while( n < size){
+			n += in.read(buffer, n, size-n);
+		}
+		return buffer;
+	}
+
+	private static Map<String, String> readHeaders(InputStream in) throws IOException {
+		
+		Map<String, String> headers = new HashMap<String, String>();
+		String headerString;
+		while( !(headerString = readLine(in)).equals("") ){
+			String[] ans = headerString.split(": ");
+			headers.put(ans[0] , ans[1]);
+		}
+		
+		return headers;
+	}
+	
+    /**
+     * Reads one message from the HTTP header
+     */
+    private static String readLine(InputStream in ) throws IOException {
+
+      StringBuffer sb = new StringBuffer() ;
+
+      int c ;
+      while( (c = in.read() ) >= 0 ) {
+        if( c == '\r' ) continue ;
+        if( c == '\n' ) break ;
+        sb.append( new Character( (char)c) ) ;
+      }
+      return sb.toString() ;
+    }
 
 }
