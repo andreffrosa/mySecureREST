48bc439afa1b1ef3c1d211d84687cc45e981abf8
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2019-05-22 12:41:09 +0100

    Added method to get query and split path and query

--- src/util/URI_Utils.java
+++ src/util/URI_Utils.java
@@ -1,8 +1,11 @@
 package util;
 
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLEncoder;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class URI_Utils {
 
@@ -15,12 +18,35 @@ public class URI_Utils {
 	}
 	
 	public static String[] splitPath(String path_with_query_params) {
-		// TODO
-		return null;
+		
+		if(path_with_query_params == null)
+			return null;
+		
+		String[] parts = path_with_query_params.split("?", 1);
+		 
+		if(parts.length > 1)
+			return parts;
+		else
+			return new String[] {parts[0], ""};
+		
 	}
 	
-	public static List<String> parseQueryParams(String query_params_string) {
-		// TODO
-		return null;
-	}
+    public static Map<String, String> parseParams(String query) throws MalformedURLException{
+    	
+    	Map<String, String> result = new HashMap<>();
+        if (query == null)
+            return result;
+
+        String[] params = new URL(query).getQuery().split("&");
+        for(String param: params) {
+        	String[] paramAndValue = param.split("=");
+        	if(paramAndValue.length > 1) 
+        		result.put(paramAndValue[0], paramAndValue[1]);
+        	else
+        		result.put(paramAndValue[0], "");
+        		
+        }
+    
+    	return result;
+    }
 }
