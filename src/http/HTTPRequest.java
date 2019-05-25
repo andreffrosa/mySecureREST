b8b71b0825835861dff05446e20e41d6ab75016f
andreffrosa <af.rosa@campus.fct.unl.pt>
2019-05-25 14:23:26 +0100

    Update HTTPRequest.java

--- src/http/HTTPRequest.java
+++ src/http/HTTPRequest.java
@@ -4,6 +4,10 @@ import java.util.Map;
 
 public class HTTPRequest extends HTTPMessage {
 	
+	private static final int METHOD = 0;
+	private static final int PATH = 1;
+	private static final int REQUEST_HTTP_VERSION = 2;
+	
 	private static final String REQUEST_PATTERN = "%s %s HTTP/%s" + SEPARATOR;
 
 	private String method;
@@ -33,6 +37,23 @@ public class HTTPRequest extends HTTPMessage {
 	
 	protected String getFirstLine() {
 		return String.format(REQUEST_PATTERN, method, path, version).toString();
+	}
+	
+	public static HTTPRequest desserializeRequest(InputStream in) throws IOException {
+
+		Map<String, String> headers ;
+
+		String[] responseStatus = readLine(in).split(" "); // [Method, Path, HttpVersion]	
+
+		//Process headers
+		headers = readHeaders(in);
+
+		byte[] buffer = extractBody(in, headers);
+
+		return new HTTPRequest( responseStatus[METHOD], 
+				        responseStatus[PATH], 
+				        responseStatus[REQUEST_HTTP_VERSION].replace("HTTP/", ""), 
+				        headers, buffer, headers.get("Content-Type"));
 	}
 	
 }
