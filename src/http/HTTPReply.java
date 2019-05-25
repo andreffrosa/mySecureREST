c7aa0648b507f578f4f478bb3406e91485c1d988
andreffrosa <af.rosa@campus.fct.unl.pt>
2019-05-25 14:21:19 +0100

    Update HTTPReply.java

--- src/http/HTTPReply.java
+++ src/http/HTTPReply.java
@@ -3,6 +3,10 @@ package http;
 import java.util.Map;
 
 public class HTTPReply extends HTTPMessage {
+	
+	private static final int RESPONSE_HTTP_VERSION = 0;
+	private static final int CODE = 1;
+	private static final int MESSAGE = 2;
 
 	private static final String REPLY_PATTERN = "HTTP/%s %d %s" + SEPARATOR;
 	
@@ -34,6 +38,22 @@ public class HTTPReply extends HTTPMessage {
 	
 	public void setStatusMessage(String status_message) {
 		this.status_message = status_message;
+	}
+	
+	public static HTTPReply desseralizeReply(InputStream in) throws IOException {
+
+		Map<String, String> headers = new HashMap<>();
+
+		String[] responseStatus = readLine(in).split(" ", 3); // [HttpVersion, Code, Message]	
+
+		headers = readHeaders(in);
+
+		byte[] buffer = extractBody(in, headers);
+
+		return new HTTPReply(responseStatus[RESPONSE_HTTP_VERSION].replace("HTTP/", ""),
+				     Integer.parseInt(responseStatus[CODE]),
+				     responseStatus[MESSAGE], 
+				     headers, buffer, headers.get("Content-Type"));
 	}
 
 }
